39521d4b650b912bedd8e0933f26c64cc149797e
pchui38 <pchui38@hotmail.com>
2014-02-05 23:27:42 +1100

    created simple server

---
+++ server.js
@@ -0,0 +1,22 @@
+/*
+// Setup Web Server
+var http = require('http');
+
+http.createServer(function(req,res) {
+	res.writeHead(200,{'Content-type':'text/plain'});	// 200 = no error status
+	res.end('Hello!');
+}).listen(1337, '127.0.0.1');
+console.log('Webserver has started');
+*/
+
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req,res) {
+	fs.readFile('index.html', function(err,data) {
+		res.writeHead(200,{'Content-type':'text/html'});	// 200 = no error status
+		res.end(data);
+	});
+
+}).listen(1337, '127.0.0.1');
+console.log('Webserver has started');
